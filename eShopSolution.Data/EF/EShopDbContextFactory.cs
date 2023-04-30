462647c5e9ffcd2c4d1b29bcbc1566c9914431d4
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-04-30 22:16:01 +0700

    migration

---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json").Build();
+            var connectionString = configuration.GetConnectionString("EShopSolutionDb");
+
+            var optionBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionBuilder.UseSqlServer(connectionString);
+            return new EShopDbContext(optionBuilder.Options);
+        }
+    }
+}
